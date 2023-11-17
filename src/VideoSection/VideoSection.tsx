a29d70dc04259f23831e511336d759a48b01cc88
emanuelcardozo <leuname.01@gmail.com>
2023-11-16 21:18:10 -0300

    Adding video poster

--- src/VideoSection/VideoSection.tsx
+++ src/VideoSection/VideoSection.tsx
@@ -3,7 +3,7 @@ import './VideoSection.css'
 function VideoSection() {
   return (
     <section className="video-container">
-      <video autoPlay muted loop playsInline preload="yes">
+      <video autoPlay muted loop playsInline poster='/Logo_with_name.png'>
         <source src="/VirtualGym.webm" type="video/webm" />
         Tu navegador no soporta el elemento de video.
       </video>
